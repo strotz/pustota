9b916e5e2f5504da1d16ccfc6364c4c6a832169b
Alexei Strots <alexei_strots@agilent.com>
2014-06-17 14:38:20 -0700

    delete source for PMB - default .NET serialization does not work due to need for partial tree update

--- src/Pustota.Maven.Base/Serialization/IProjectSerializerWithUpdate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Pustota.Maven.Base.Data;
-
-namespace Pustota.Maven.Base.Serialization
-{
-	public interface IProjectSerializerWithUpdate : IProjectSerializer
-	{
-		string UpdateContent(Project project, string content);
-	}
-}
