2e258a13061ff70013158a8ae333c6bf8c1e3898
Alexei Strots <strotz@gmail.com>
2014-07-28 14:13:05 -0700

    couple test for cascade switch to snapshot

--- src/Pustota.Maven.Base.Tests/Actions/CascadeSwitchToSnapshotTests.cs
+++ src/Pustota.Maven.Base.Tests/Actions/CascadeSwitchToSnapshotTests.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using Moq;
 using NUnit.Framework;
+using Pustota.Maven.Actions;
+using Pustota.Maven.Models;
+using Pustota.Maven.Serialization.Data;
 
 namespace Pustota.Maven.Base.Tests.Actions
 {
@@ -21,9 +24,82 @@ namespace Pustota.Maven.Base.Tests.Actions
 		{
 		}
 
-		[Test, Ignore]
-		public void Test()
+		[Test]
+		public void SingleProjectTest()
 		{
+			var project = new Project
+			{
+				GroupId = "group",
+				ArtifactId = "a",
+				Version = "1.0.0"
+			};
+
+			var repo = new Mock<IProjectsRepository>();
+			repo.SetupGet(r => r.AllProjects).Returns(new IProject[] { project });
+
+			var action = new CascadeSwitchAction(repo.Object);
+
+			action.ExecuteFor(project);
+
+			Assert.That(project.Version, Is.EqualTo("1.0.1-SNAPSHOT"));
+			Assert.True(project.ReferenceOperations().IsSnapshot);
+		}
+
+		[Test]
+		public void SingleProjectNoSwitchTest()
+		{
+			var project = new Project
+			{
+				GroupId = "group",
+				ArtifactId = "a",
+				Version = "1.0.1-SNAPSHOT"
+			};
+
+			var repo = new Mock<IProjectsRepository>();
+			repo.SetupGet(r => r.AllProjects).Returns(new IProject[] { project });
+
+			var action = new CascadeSwitchAction(repo.Object);
+
+			action.ExecuteFor(project);
+
+			Assert.That(project.Version, Is.EqualTo("1.0.1-SNAPSHOT"));
+			Assert.True(project.ReferenceOperations().IsSnapshot);
+		}
+
+		[Test]
+		public void TwoProjectsParentTest()
+		{
+			var projectA = new Project
+			{
+				GroupId = "group",
+				ArtifactId = "a",
+				Version = "1.0.0"
+			};
+
+			var projectB = new Project
+			{
+				GroupId = "group",
+				ArtifactId = "b",
+				Version = "1.0.1",
+				Parent = new ParentReference{
+					GroupId = "group",
+					ArtifactId = "a",
+					Version = "1.0.0",
+				},
+			};
+
+			var repo = new Mock<IProjectsRepository>();
+			repo.SetupGet(r => r.AllProjects).Returns(new IProject[] { projectA, projectB });
+
+			var action = new CascadeSwitchAction(repo.Object);
+
+			action.ExecuteFor(projectA);
+
+			Assert.That(projectA.Version, Is.EqualTo("1.0.1-SNAPSHOT"));
+			Assert.True(projectA.ReferenceOperations().IsSnapshot);
+
+			Assert.That(projectB.Version, Is.EqualTo("1.0.2-SNAPSHOT"));
+			Assert.True(projectB.ReferenceOperations().IsSnapshot);
 		}
 	}
 }
